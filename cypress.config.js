eb75135203ed5c619d71ffc55c374165c5ab3519
venoxik <102427744+venoxik@users.noreply.github.com>
2023-04-15 19:37:46 +0200

    added todo tests

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
-    baseUrl: "https://todomvc.com/examples/vue/?fbclid=IwAR023Rd-GFJn55HhQf3CPivCpJOWs2catatb709x_VxrhsS3Qt3lZ9zN5xw",
+    baseUrl: "https://todomvc.com/examples/vue",
     chromeWebSecurity: false,
     defaultCommandTimeout: 10000
   },
