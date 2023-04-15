e5c09d805387ce00d266b6e7d4627fed69f55335
venoxik <102427744+venoxik@users.noreply.github.com>
2023-04-15 17:27:02 +0200

    todos

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    baseUrl: "https://todomvc.com/examples/vue/?fbclid=IwAR023Rd-GFJn55HhQf3CPivCpJOWs2catatb709x_VxrhsS3Qt3lZ9zN5xw",
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 10000
   },
 });
